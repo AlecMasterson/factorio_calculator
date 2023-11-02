032656af06cd1ed602bfdd48bed81f393b7ae743
Alec Masterson <alecjmasterson@gmail.com>
2023-11-01 19:52:11 -0500

    latest reactflow logic

--- src/Flow.tsx
+++ src/Flow.tsx
@@ -1,61 +1,69 @@
 import React, {useCallback} from 'react';
 import ReactFlow, {addEdge, Connection, Edge, Node as FlowNode, useNodesState, useEdgesState} from 'reactflow';
-import Node from './types/Node';
-import CustomNode from './CustomNode';
 import 'reactflow/dist/style.css';
+import {useDispatch} from 'react-redux';
+import NodeConstant from './components/node/Constant';
+import NodeMachine from './components/node/Machine';
+import {Node} from './types/Node';
+import {NodeType} from './types/NodeType';
+import {updatePosition} from './Store';
 
-const NodeTypes = {CustomNode};
+const NodeTypes = {NodeConstant, NodeMachine};
 
 interface GraphNode {
-    attributes: Node,
-    key: string
+  attributes: Node;
+  key: string;
 }
 
 interface GraphProps {
-    nodes: GraphNode[];
+  nodes: GraphNode[];
 }
 
 interface FlowProps {
-    graph: GraphProps;
-    onAddEdge: (connection: Connection) => boolean;
+  graph: GraphProps;
+  onAddEdge: (connection: Connection) => boolean;
 }
 
 export default function Flow(props: FlowProps): React.ReactElement {
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const dispatch = useDispatch();
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+
+  React.useEffect(() => {
+    if (!props.graph.nodes) {
+      return;
+    }
+
     console.log('Flow Props', props);
-    console.log('Flow Edges', edges);
-    console.log('Flow Nodes', nodes);
+    setNodes(props.graph.nodes.map((node: GraphNode): FlowNode => ({
+      data: {},
+      id: node.key,
+      position: node.attributes.position,
+      type: node.attributes.type === NodeType.CONSTANT ? 'NodeConstant' : 'NodeMachine'
+    })));
+  }, [props.graph]);
 
-    React.useEffect(() => {
-        if (!props.graph.nodes) {
-            return;
-        }
+  const onConnect = useCallback((connection: Connection): void => {
+    if (props.onAddEdge(connection)) {
+      setEdges((tempEdges: Edge[]) => addEdge(connection, tempEdges));
+    }
+  }, [setEdges]);
 
-        setNodes(props.graph.nodes.map((node: GraphNode): FlowNode => ({
-            data: {},
-            id: node.key,
-            position: node.attributes.position,
-            type: typeof node.attributes.rate === 'number' ? '' : 'CustomNode'
-        })));
-    }, [props.graph]);
+  const onNodeDragStop = useCallback((_: any, x: {id: string, position: {x: number, y: number}}): void => {
+    dispatch(updatePosition({nodeId: x.id, position: x.position}));
+  }, [onNodesChange]);
 
-    const onConnect = useCallback((connection: Connection): void => {
-        if (props.onAddEdge(connection)) {
-            setEdges((tempEdges: Edge[]) => addEdge(connection, tempEdges));
-        }
-    }, [setEdges]);
-
-    return (
-      <div style={{height: '100vh', width: '100vw'}}>
-        <ReactFlow
-          edges={edges}
-          nodes={nodes}
-          nodeTypes={NodeTypes}
-          onConnect={onConnect}
-          onEdgesChange={onEdgesChange}
-          onNodesChange={onNodesChange}
-        />
-      </div>
-    );
+  return (
+    <div style={{height: '100vh', width: '100vw'}}>
+      <ReactFlow
+        edges={edges}
+        nodes={nodes}
+        nodeTypes={NodeTypes}
+        onConnect={onConnect}
+        onEdgesChange={onEdgesChange}
+        onNodeDragStop={onNodeDragStop}
+        onNodesChange={onNodesChange}
+      />
+    </div>
+  );
 }
