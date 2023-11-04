6746be6096d5037cc0fc09210d0c3dd138545b48
Alec Masterson <alecjmasterson@gmail.com>
2023-11-04 09:45:31 -0500

    add methods to edges store

--- src/store/StoreEdges.ts
+++ src/store/StoreEdges.ts
@@ -1,9 +1,20 @@
 import {createSlice, Slice} from '@reduxjs/toolkit';
+import {cloneDeep} from 'lodash';
 
 export const SliceEdges: Slice = createSlice({
   initialState: {},
   name: 'edges',
-  reducers: {}
+  reducers: {
+    addEdge: (state: string[], params: {payload: {edge: string}}): string[] => {
+      const edges: string[] = cloneDeep(state);
+      edges.push(params.payload.edge);
+
+      return edges;
+    },
+    setEdges: (_: string[], params: {payload: {edges: string[]}}): string[] => {
+      return params.payload.edges;
+    }
+  }
 });
 
-export const {} = SliceEdges.actions;
+export const {addEdge, setEdges} = SliceEdges.actions;
