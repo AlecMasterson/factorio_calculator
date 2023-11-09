445ec00490f79f0d64fee1f91824414a8683501c
Alec Masterson <alecjmasterson@gmail.com>
2023-11-09 13:01:54 -0600

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,71 @@
+// @ts-ignore
 import React from 'react';
 import Graph from 'graphology';
-import {min, sum, update} from 'lodash';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {Dispatch} from '@reduxjs/toolkit';
 import {Connection} from 'reactflow';
-import Store, {set, updateIngredient, updateProduct} from './Store';
-import {ItemIngredient, ItemProduct} from './types/Item';
-import {Node} from './types/Node';
+import {Node, NodeConstant, NodeMachine} from './types/Node';
+import {NodeMap} from './types/NodeMap';
+import {addEdge} from './store/StoreEdges';
 import Flow from './Flow';
+import Util from './Util';
 
-const graph = new Graph({allowSelfLoops: false, multi: true, type: 'directed'});
+const graph = new Graph({multi: true, type: 'directed'});
 
-interface AppProps {
-  nodes: {[id: string]: Node};
-}
-
-function App(props: AppProps): React.ReactElement {
-  const dispatch = useDispatch();
-  const [graphJson, setGraphJson] = React.useState<{nodes: any[]}>({nodes: []});
+function App(): React.ReactElement {
+  const dispatch: Dispatch = useDispatch();
+  const edges: string[] = useSelector((state: {edges: string[]}): string[] => state.edges);
+  const nodes: NodeMap = useSelector((state: {nodes: NodeMap}): NodeMap => state.nodes);
 
   React.useEffect(() => {
     graph.clear();
-    dispatch(set(props.nodes));
 
-    Object.values(props.nodes).forEach((node: Node): void => {
+    // @ts-ignore
+    Object.values(nodes).forEach((node: Node): void => {
       graph.addNode(node.id, {...node});
     });
-
-    setGraphJson(graph.export());
-  }, [props.nodes]);
+  }, [ nodes]);
 
   React.useEffect(() => {
-    graph.on('edgeAdded', ({key, source, target}) => {
-      const keyy = key.split(',')[0].split('-')[1];
-      graph.updateNodeAttribute(source, 'products', (temp: {[id: string]: ItemProduct}): {[id: string]: ItemProduct} => {
-        const newProducts: {[id: string]: ItemProduct} = {...temp};
+    graph.on('edgeAdded', (params: {key: string}): void => Util.graphEdgeAdded(dispatch, graph, params));
 
-        const product: ItemProduct = {...newProducts[keyy]};
-        product.targets = [...product.targets, target];
-
-        dispatch(updateProduct({nodeId: source, product}));
-        return {...newProducts, [keyy]: product};
-      });
-    });
-
-    graph.on('nodeAttributesUpdated', ({attributes, key, name}) => {
-      console.log('Node attribute Updated', attributes, key, name);
-
-      if (name === 'products') {
-        (Object.values(attributes.products) as ItemProduct[]).forEach((product: ItemProduct): void => {
-          const rateShared: number = product.rate / product.targets.length;
-
-          product.targets.forEach((target: string): void => {
-            graph.updateNodeAttribute(target, 'ingredients', (temp: {[id: string]: ItemIngredient}): {[id: string]: ItemIngredient} => {
-              const newIngredients: {[id: string]: ItemIngredient} = {...temp};
-              console.log('Current Ingredients', newIngredients);
-              console.log('product.id', product.id);
-
-              const ingredient: ItemIngredient = {...newIngredients[product.id]};
-              console.log('ingredient', ingredient);
-              ingredient.rate = {...ingredient.rate, [attributes.id]: rateShared};
-
-              dispatch(updateIngredient({nodeId: target, ingredient}));
-              return {...newIngredients, [product.id]: ingredient};
-            });
-          });
-        });
+    graph.on('nodeAttributesUpdated', (params: any): void => {
+      if (params.name === 'ingredients') {
+        Util.graphUpdatedIngredients(dispatch, graph, params.attributes as NodeMachine);
       }
 
-      if (name === 'ingredients') {
-        const crafts: number[] = (Object.values(attributes.ingredients) as ItemIngredient[]).map((ingredient: ItemIngredient): number => {
-          return sum(Object.values(ingredient.rate)) / ingredient.value;
-        });
-        const craftsMin: number = min(crafts);
+      if (params.name === 'products') {
+        Util.graphUpdatedProducts(dispatch, graph, params.attributes as NodeMachine);
+      }
 
-        graph.updateNodeAttribute(attributes.id, 'products', (temp: {[id: string]: ItemProduct}): {[id: string]: ItemProduct} => {
-          let newProducts: {[id: string]: ItemProduct} = {...temp};
-
-          Object.values(newProducts).forEach((product: ItemProduct): void => {
-            const newProduct: ItemProduct = {...product};
-            newProduct.rate = craftsMin*newProduct.value;
-
-            dispatch(updateProduct({nodeId: attributes.id, product: newProduct}));
-            newProducts = {...newProducts, [newProduct.id]: newProduct};
-          });
-
-          return newProducts;
-        });
+      if (params.name === 'targets') {
+        Util.graphUpdatedTargets(dispatch, graph, params.attributes as NodeConstant);
       }
     });
   }, []);
 
-  function addEdge(connection: Connection): boolean {
-    const sourceName: string = graph.getNodeAttribute(connection.source, 'products')[connection.sourceHandle as string].name;
-    const targetName: string = graph.getNodeAttribute(connection.target, 'ingredients')[connection.targetHandle as string].name;
-    if (sourceName !== targetName) {
+  const graphAddEdge: (connection: Connection) => boolean = React.useCallback((connection: Connection): boolean => {
+    try {
+      const canConnect: boolean = Util.graphCanConnect(graph, connection);
+      if (!canConnect) {
+        return false;
+      }
+
+      const keyId: string = `${connection.source}-${connection.sourceHandle},${connection.target}-${connection.targetHandle}`;
+
+      graph.addEdgeWithKey(keyId, connection.source, connection.target);
+      dispatch(addEdge({edge: keyId}));
+
+      return true;
+    } catch (error: unknown) {
+      console.error('Failed to Add Edge', error);
       return false;
     }
-
-    const keyId: string = `${connection.source}-${connection.sourceHandle},${connection.target}-${connection.targetHandle}`;
-    graph.addEdgeWithKey(keyId, connection.source, connection.target);
-    return true;
-  }
+  }, []);
 
   return (
     <Flow
-      graph={graphJson}
-      onAddEdge={addEdge}
+      onAddEdge={graphAddEdge}
     />
   );
 }
