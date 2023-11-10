22fd170116bbf7b6d7caebf6288cf8dbe608a90b
Alec Masterson <alecjmasterson@gmail.com>
2023-11-09 19:52:12 -0600

    constant

--- src/components/node/Constant.tsx
+++ src/components/node/Constant.tsx
@@ -1,6 +1,7 @@
+// @ts-ignore
 import React from 'react';
-import {Handle, NodeProps, Position} from 'reactflow';
 import {useSelector} from 'react-redux';
+import {Handle, NodeProps, Position} from 'reactflow';
 import {Card, CardContent} from '@mui/material';
 import {NodeConstant} from '../../types/Node';
 import {NodeMap} from '../../types/NodeMap';
@@ -9,22 +10,28 @@ import {NodeType} from '../../types/NodeType';
 export default function Constant(props: NodeProps): React.ReactElement {
   const nodes: NodeMap = useSelector((state: {nodes: NodeMap}): NodeMap => state.nodes);
 
+  if (!(props.id in nodes)) {
+    return null;
+  }
   if (nodes[props.id].type !== NodeType.CONSTANT) {
     throw new Error('Attempting to Render Non-Constant Node as a Constant Node');
   }
+
   const node: NodeConstant = nodes[props.id] as NodeConstant;
 
   return (
     <Card variant='outlined'>
       <CardContent>
-        <h2>{node.name}</h2>
-        <span>Output Rate: {node.rate}</span>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <span>{node.name}</span>
+          <span>Rate: {node.rate.toFixed(2)}</span>
 
-        <Handle
-          id={node.id}
-          position={Position.Right}
-          type='source'
-        />
+          <Handle
+            id={node.id}
+            position={Position.Right}
+            type='source'
+          />
+        </div>
       </CardContent>
     </Card>
   );
